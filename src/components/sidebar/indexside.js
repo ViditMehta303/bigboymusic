c96aa725d51522757e4d1815224220d1b1111259
ViditMehta303 <Viditmehta18@gmail.com>
2025-07-24 13:47:22 -0700

    new cahnge

--- src/components/sidebar/indexside.js
+++ src/components/sidebar/indexside.js
@@ -4,7 +4,7 @@ import "./sidebar.css";
 export default function Sidebar() {
   return (
     <div classname="sidebar-container">
-        <image src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ebay.ca%2Fitm%2F324828945961&psig=AOvVaw0MhIswyTEvJMWQweGaQ0oU&ust=1752124832574000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjT4eODr44DFQAAAAAdAAAAABAE" className='profile-img' alt="profile" />
+        <image src="C:\Users\Rekha Mehta\OneDrive - University of Victoria\Pictures\weekend poster - Copy.jpg" className='profile-img' alt="profile" />
         <div>
             <sidebarbutton/>
             <sidebarbutton/>
